9cec89bf20c9b895f9d536ddbe38f7e728205e51
HKIT301_이혜성 <hyeseong43@gmail.com>
2020-04-24 08:45:23 +0900

    짝홀_코드잇

---
+++ 파이썬/프로그래밍기초/시작하기/추상화/9_짝홀.py
@@ -0,0 +1,11 @@
+def is_evenly_divisible(number):
+    if number%2 != 0:
+        return False
+    else:
+        return True
+    
+print(is_evenly_divisible(3))
+# print(is_evenly_divisible(7))
+# print(is_evenly_divisible(8))
+
+lambda x : print('짝') if i%2 != 0 else print('홀'), for i in ran
